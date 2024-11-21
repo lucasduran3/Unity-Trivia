1bc506e7e7cef60d81a81a8ae9fe163d964119b6
Lucas Duran <100479433+lucasduran3@users.noreply.github.com>
2024-11-21 16:36:29 -0300

    Initialize feature/main_game

---
+++ Assets/Scripts/TriviaSelection.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using Supabase;
+using Supabase.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Postgrest.Models;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TriviaSelection : MonoBehaviour
+{
+    string supabaseUrl = "https://zkyndewjwgeydidqerqx.supabase.co";
+    string supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpreW5kZXdqd2dleWRpZHFlcnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwMzM1MjQsImV4cCI6MjAyNjYwOTUyNH0.Mq1PucFgRp2Vyxv3tFZs709iSRmj2jV8oXvlzLmGBP0";
+
+    Supabase.Client clientSupabase;
+
+    List<trivia> trivias = new List<trivia>();
+    [SerializeField] TMP_Dropdown _dropdown;
+
+    public DatabaseManager databaseManager;
+
+    async void Start()
+    {
+        clientSupabase = new Supabase.Client(supabaseUrl, supabaseKey);
+
+        await SelectTrivias();
+        PopulateDropdown();
+    }
+
+    async Task SelectTrivias()
+    {
+        var response = await clientSupabase
+            .From<trivia>()
+            .Select("*")
+            .Get();
+
+        if (response != null)
+        {
+            trivias = response.Models;
+            //Debug.Log("Trivias seleccionadas: " + trivias.Count);
+            //foreach (var trivia in trivias)
+            //{
+            //    Debug.Log("ID: " + trivia.id + ", Categoría: " + trivia.category);
+            //}
+        }
+
+    }
+
+    void PopulateDropdown()
+    {
+        
+        _dropdown.ClearOptions();
+
+        List<string> categories = new List<string>();
+
+        foreach (var trivia in trivias)
+        {
+            categories.Add(trivia.category);
+        }
+
+        _dropdown.AddOptions(categories);
+    }
+
+    public void OnStartButtonClicked()
+    {
+        int selectedIndex = _dropdown.value;
+        string selectedTrivia = _dropdown.options[selectedIndex].text;
+
+        PlayerPrefs.SetInt("SelectedIndex", selectedIndex+1);
+        PlayerPrefs.SetString("SelectedTrivia", selectedTrivia);
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
